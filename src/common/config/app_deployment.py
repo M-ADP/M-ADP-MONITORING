07240bdd549a563fc2d1188bcdadc5960361c253
pdh0128 <pdh0128a@gmail.com>
2026-03-06 00:02:39 +0900

    feat(config): app deployment config 추가

--- src/common/config/app_deployment.py
+++ src/common/config/app_deployment.py
@@ -3,11 +3,11 @@ from pydantic_settings import BaseSettings
 from src.common.const.vault import VAULT_ENV_FILE
 
 
-class PrometheusConfig(BaseSettings):
-    url: str = "http://localhost:9090"
+class AppDeploymentConfig(BaseSettings):
+    url: str = "http://localhost:8000"
 
     class Config:
-        env_prefix = "PROMETHEUS_"
+        env_prefix = "APP_DEPLOYMENT_"
         env_file = VAULT_ENV_FILE
         env_file_encoding = "utf-8"
         extra = "ignore"

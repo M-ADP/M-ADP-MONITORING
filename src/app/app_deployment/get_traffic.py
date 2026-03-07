7e6c5069f34c544d17d6e92f9029fc850ab6474c
pdh0128 <pdh0128a@gmail.com>
2026-03-07 17:49:24 +0900

    fix(App Deployment): get app deployment dependencies

--- src/app/app_deployment/get_traffic.py
+++ src/app/app_deployment/get_traffic.py
@@ -1,14 +1,17 @@
+from fastapi import Depends
+
 from src.app.base_use_case import BaseUseCase
 from src.common.schema.request import TrafficRangeRequest
 from src.core.app_deployment.model import AppDeployment, AppDeploymentTraffic
 from src.core.app_deployment.monitoring import AppDeploymentMonitoringClient
+from src.dependencies.get_app_deployment_monitoring import get_app_deployment_monitoring
 
 
 class GetAppDeploymentTrafficUseCase(BaseUseCase):
 
     def __init__(
             self,
-            monitoring_client : AppDeploymentMonitoringClient
+            monitoring_client : AppDeploymentMonitoringClient = Depends(get_app_deployment_monitoring)
     ) -> None:
         self.monitoring_client = monitoring_client
 
